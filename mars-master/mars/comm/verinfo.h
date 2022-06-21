2764fdd149dd4f3048bd1b8785dc696228aa4599
王春祥 <wangchunxiang@tojoy.com>
2022-06-21 14:48:33 +0800

    s

---
+++ mars-master/mars/comm/verinfo.h
@@ -0,0 +1,11 @@
+
+#ifndef Mars_verinfo_h
+#define Mars_verinfo_h
+
+#define MARS_REVISION ""
+#define MARS_PATH ""
+#define MARS_URL ""
+#define MARS_BUILD_TIME "2021-05-12 10:35:42"
+#define MARS_TAG ""
+
+#endif
